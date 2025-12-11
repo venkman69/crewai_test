f03510d223692fc6fee6ca37213df681864ddb37
Narayan Natarajan <narayan.natarajan@gmail.com>
2025-12-10 22:45:54 -0500

    improved visual and updated to use new crew setup

--- src/job_dash.py
+++ src/job_dash.py
@@ -8,17 +8,23 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 # TODO: select a previous uploaded job from jobs.
-# TODO: Move missing skills above the matching skills.
 # TODO: Check for failure to access/download the job url and inform user.
 # TODO: Display the saved crew output files in an expandable section.
 
+# -- initialize env etc --
+if "initialized" not in st.session_state:
+    with st.spinner("Initializing..."):
+        load_dotenv()
+        st.session_state.initialized = True
+        utils.make_work_dirs()
 
-if os.getenv("GEMINI_API_KEY") is None:
-    load_dotenv()
-    if os.getenv("GEMINI_API_KEY") is None:
-        raise ValueError("GEMINI_API_KEY not found in environment variables")
-    else:
-        print("GEMINI_API_KEY found in environment variables")
+        # load storage locations from .env into session state
+        resume_storage_dir = os.getenv("RESUME_STORAGE_DIR")
+        job_storage_dir = os.getenv("JOB_STORAGE_DIR")
+        crew_output_storage_dir = os.getenv("CREW_OUTPUT_STORAGE_DIR")
+        st.session_state.resume_storage_dir = resume_storage_dir
+        st.session_state.job_storage_dir = job_storage_dir
+        st.session_state.crew_output_storage_dir = crew_output_storage_dir
 
 st.set_page_config(page_title="Resume Job Scorer", layout="wide")
 
@@ -26,21 +32,13 @@ st.title("Resume Job Scorer")
 st.markdown("Upload your resume and provide a job URL to see how well you match!")
 
 
-def rescan_resume_folder():
-    resume_files = os.listdir("./parsed_files")
-    resume_files.sort(
-        key=lambda x: os.path.getmtime(os.path.join("./parsed_files", x)), reverse=True
-    )
-    return resume_files
-
-
 # Sidebar for inputs
 with st.sidebar:
-    # list last 3 uploaded resumes in ./parsed_files in a dropdown  sort by date newest to oldest
+    # list last 3 uploaded resumes in ./work/resumes in a dropdown  sort by date newest to oldest
     if st.button("Rescan Resume Folder"):
-        resume_files = rescan_resume_folder()
+        resume_files = utils.scan_resume_folder(st.session_state.resume_storage_dir)
     else:
-        resume_files = rescan_resume_folder()
+        resume_files = utils.scan_resume_folder(st.session_state.resume_storage_dir)
     resume_files = ["Upload Resume (PDF)"] + resume_files[:3]
     resume_file = st.selectbox("Resume", resume_files, index=0)
     if resume_file == "Upload Resume (PDF)":
@@ -49,16 +47,20 @@ with st.sidebar:
         )
         previous_resume = False
     else:
-        resume_file = os.path.join("./parsed_files", resume_file)
+        resume_file = os.path.join(st.session_state.resume_storage_dir, resume_file)
         previous_resume = True
-    resume_caching = st.checkbox("Caching", key="resume_caching", value=True)
+    resume_caching = st.checkbox(
+        "Use cached result if available", key="resume_caching", value=True
+    )
 
     job_url = st.text_input(
         "Job URL", placeholder="https://www.linkedin.com/jobs/view/..."
     )
     job_text = st.text_area("Job Text", placeholder="Paste job description here")
 
-    job_caching = st.checkbox("Caching", key="job_caching", value=True)
+    job_caching = st.checkbox(
+        "Use cached result if available", key="job_caching", value=True
+    )
     analyze_button = st.button("Analyze Match")
 
     st.divider()
@@ -99,19 +101,14 @@ if analyze_button:
                 st.success(
                     f"**Resume analysis complete!**\n\n"
                     f"Analysis saved to: `{resume_analysis_path}`\n\n"
-                    f"Resume stored as: `./parsed_files/{resume_parsed_filename}.txt`"
+                    f"Resume stored as: `{st.session_state.resume_storage_dir}/{resume_parsed_filename}.txt`"
                 )
-            with st.spinner("Analysing job..."):
-                job_analysis, job_analysis_path = (
-                    resume_job_scorer.job_skill_analyser_crew(job_url, job_caching)
-                )
-                st.success(
-                    f"**Job analysis complete!**\n\nAnalysis saved to: `{job_analysis_path}`"
-                )
-            with st.spinner("Deciding..."):
-                final_decision, final_decision_path = (
-                    resume_job_scorer.compare_and_decide_crew(
-                        resume_analysis.raw, job_analysis.raw, job_caching
+            with st.spinner("Analyzing Job vs Resume skills and Deciding..."):
+                final_decision, final_decision_path, job_details = (
+                    resume_job_scorer.job_analysis_and_decision_crew(
+                        job_url,
+                        resume_analysis,
+                        job_caching,
                     )
                 )
                 st.success(
@@ -130,41 +127,40 @@ if analyze_button:
             st.header("Analysis Results")
 
             # Job Details
-            if "job_posting_details" in result_json:
-                details = result_json["job_posting_details"]
-                org = details.get("organization")
-                url = details.get("job_url")
-                source = details.get("job_source")
-                role_summary = details.get("role_summary")
+            org = result_json.get("organization")
+            job_summary = result_json.get("job_summary")
 
-                st.subheader("Job Details")
-                st.write(
-                    f"""
-                    <div style="padding: 15px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9; margin-bottom: 15px;">
-                        <h3 style="margin-top: 0; color: #333;">{org}</h3>
-                        <p style="margin-bottom: 10px;"><strong>Source:</strong> {source}</p>
-                        <p style="margin-bottom: 10px;"><strong>Role Summary:</strong> {role_summary}</p>
-                        <a href="{url}" target="_blank" style="text-decoration: none;">
-                            <div style="display: inline-block; background-color: #007bff; color: white; padding: 8px 16px; border-radius: 4px; font-weight: bold;">
-                                View Job Posting
-                            </div>
-                        </a>
-                    </div>
-                    """,
-                    unsafe_allow_html=True,
+            url = job_details.get("job_url")
+            source = job_details.get("job_source")
+
+            st.subheader("Job Details")
+            st.write(
+                f"""
+                <div style="padding: 15px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9; margin-bottom: 15px;">
+                    <h3 style="margin-top: 0; color: #333;">{org}</h3>
+                    <p style="margin-bottom: 10px;"><strong>Source:</strong> {source}</p>
+                    <p style="margin-bottom: 10px;"><strong>Role Summary:</strong> {job_summary}</p>
+                    <a href="{url}" target="_blank" style="text-decoration: none;">
+                        <div style="display: inline-block; background-color: #007bff; color: white; padding: 8px 16px; border-radius: 4px; font-weight: bold;">
+                            View Job Posting
+                        </div>
+                    </a>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+
+            decision = result_json.get("decision")
+            reason = result_json.get("reason")
+            st.subheader("Resume vs Job Decision")
+            if decision == "Pass":
+                st.success(
+                    f"Candidate resume is a good fit for the Job! Reason: {reason}"
                 )
-
-            if "resume_vs_job_decision" in result_json:
-                decision = result_json["resume_vs_job_decision"]
-                st.subheader("Resume vs Job Decision")
-                if decision["decision"] == "Pass":
-                    st.success(
-                        f"Candidate resume is a good fit for the Job! Reason: {decision['reason']}"
-                    )
-                else:
-                    st.error(
-                        f"Candidate resume is not a good fit for the Job! Reason: {decision['reason']}"
-                    )
+            else:
+                st.error(
+                    f"Candidate resume is not a good fit for the Job! Reason: {reason}"
+                )
 
             # Score Section
             if "score" in result_json:
@@ -218,6 +214,17 @@ if analyze_button:
             col3, col4 = st.columns(2)
 
             with col3:
+                if (
+                    "missing_required_skills" in result_json
+                    and result_json["missing_required_skills"]
+                ):
+                    skills_list = "\n".join(
+                        [
+                            f"- {skill}"
+                            for skill in result_json["missing_required_skills"]
+                        ]
+                    )
+                    st.error(f"**Missing:**\n\n{skills_list}")
                 st.subheader("Required Skills")
                 if (
                     "matching_required_skills" in result_json
@@ -231,19 +238,18 @@ if analyze_button:
                     )
                     st.success(f"**Matching:**\n\n{skills_list}")
 
+            with col4:
                 if (
-                    "missing_required_skills" in result_json
-                    and result_json["missing_required_skills"]
+                    "missing_preferred_skills" in result_json
+                    and result_json["missing_preferred_skills"]
                 ):
                     skills_list = "\n".join(
                         [
                             f"- {skill}"
-                            for skill in result_json["missing_required_skills"]
+                            for skill in result_json["missing_preferred_skills"]
                         ]
                     )
                     st.error(f"**Missing:**\n\n{skills_list}")
-
-            with col4:
                 st.subheader("Preferred Skills")
                 if (
                     "matching_preferred_skills" in result_json
@@ -256,18 +262,6 @@ if analyze_button:
                         ]
                     )
                     st.success(f"**Matching:**\n\n{skills_list}")
-
-                if (
-                    "missing_preferred_skills" in result_json
-                    and result_json["missing_preferred_skills"]
-                ):
-                    skills_list = "\n".join(
-                        [
-                            f"- {skill}"
-                            for skill in result_json["missing_preferred_skills"]
-                        ]
-                    )
-                    st.error(f"**Missing:**\n\n{skills_list}")
 
             st.divider()
 
